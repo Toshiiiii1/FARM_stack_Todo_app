bb38eadba843241f2eed3d9897c2602f0810843b
Toshi <huynhduykhoi619@gmail.com>
2023-12-06 13:42:09 +0700

    define routes

--- backend/main.py
+++ backend/main.py
@@ -16,4 +16,28 @@ app.add_middleware(
 
 @app.get("/")
 def read_root():
-    return {"Ping": "Pong"}
+    res = {
+        "Ping": "Pong",
+        "Hello": "world"
+    }
+    return res
+
+@app.get("/api/todo")
+async def get_todo():
+    return 1
+
+@app.get("/api/todo/{id}")
+async def get_todo_by_id(id):
+    return 1
+
+@app.post("/api/todo")
+async def create_todo(todo):
+    return 1
+
+@app.put("/api/todo/{id}")
+async def update_todo(id, todo):
+    return 1
+
+@app.delete("/api/todo/{id}")
+async def delete_todo(id):
+    return 1
